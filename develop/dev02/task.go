41ea988dba78b976883254721719d3093f17b7d3
Артём Прилуцких <prilutskikh@gmail.com>
2022-07-04 01:14:08 +0300

    add 10task

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+
+	"errors"
+	"fmt"
+	"strconv"
+
+	"unicode"
+)
+
 /*
 === Задача на распаковку ===
 
@@ -18,6 +27,55 @@ package main
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+var errString = errors.New("некорректная строка")
 
+const esc = '\\'
+
+func main() {
+	a := "qwe\\45" //"a4bc2d5e" => "aaaabccddddde" aaaabccddddde
+
+	val, err := Unpack(a)
+	fmt.Println(val, err)
+	//- qwe\4\5 => qwe45 (*)
+	//- qwe\45 => qwe44444 (*)
+	//- qwe\\5 => qwe\\\\\ (*)
+}
+
+func Unpack(str string) (string, error) {
+	var temp rune
+	var res []rune
+	var num int
+	var escBool bool //флаг для escape последовательностей
+
+	for i, val := range str {
+		//проверяем флаг на наличие последовательностей
+		if !escBool{
+			//проверяем является ли текущий символ цифрой или escape последовательностью
+			if unicode.IsDigit(val) || val == esc {
+				//если цифра идет первой в очереди возвращаем ошибку
+				if i == 0 {
+					return "", errString
+				}
+				if val == esc {
+					escBool = true
+					continue
+				}
+				//преобразовываем руну в цифру
+				num, _ = strconv.Atoi(string(val))
+				//добавляем предыдущую руну в результирующий срез num-1 раз
+				for i = 1; i < num; i++ {
+					res = append(res, temp)
+				}
+				continue // переходим к следующей последовательности
+			}
+		}
+
+		//если текущая руна не цифра и не escape последовательность добавляем в результирующий срез и
+		//во временную переменную
+		escBool = false
+		res = append(res, val)
+		temp = val
+	}
+
+	return string(res), nil
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,70 @@
 package main
+
+import "testing"
+
+func TestStringUnpack(t *testing.T) {
+	//- "a4bc2d5e" => "aaaabccddddde"
+	//- "abcd" => "abcd"
+	//- "45" => "" (некорректная строка)
+	//- "" => ""
+	strTestTable := []struct {
+		strTest  string
+		expected string
+	}{
+		{
+			strTest:  "a4bc2d5e",
+			expected: "aaaabccddddde",
+		},
+		{
+			strTest:  "abcd",
+			expected: "abcd",
+		},
+		{
+			strTest:  "45",
+			expected: "",
+		},
+		{
+			strTest:  "",
+			expected: "",
+		},
+	}
+
+	for _, testCase := range strTestTable {
+		result, _ := Unpack(testCase.strTest)
+
+		if result != testCase.expected {
+			t.Errorf("Некорректный результат. Ожидалось: %s, получили: %s", testCase.expected, result)
+		}
+	}
+}
+
+func TestEscapeUnpack(t *testing.T) {
+	//Дополнительное задание: поддержка escape - последовательностей
+	//- qwe\4\5 => qwe45 (*)
+	//- qwe\45 => qwe44444 (*)
+	//- qwe\\5 => qwe\\\\\ (*)
+	escTestTable := []struct {
+		escTest  string
+		expected string
+	}{
+		{
+			escTest:  "qwe\\4\\5",
+			expected: "qwe45",
+		},
+		{
+			escTest:  "qwe\\45",
+			expected: "qwe44444",
+		},
+		{
+			escTest:  "qwe\\\\5",
+			expected: "qwe\\\\\\\\\\",
+		},
+	}
+
+	for _, testCase := range escTestTable {
+		result, _ := Unpack(testCase.escTest)
+		if testCase.expected != result {
+			t.Errorf("Некорректный результат. Ожидалось: %s, получили: %s", testCase.expected, result)
+		}
+	}
+}
