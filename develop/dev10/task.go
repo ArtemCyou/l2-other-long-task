8edb2a23f701744a6027cac35c5819aac53e955d
Артём Прилуцких <prilutskikh@gmail.com>
2022-07-13 19:23:36 +0300

    add 10task

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+)
+
 /*
 === Утилита telnet ===
 
@@ -16,5 +26,74 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 */
 
 func main() {
+	timeout := flag.Duration("timeout", 1*time.Second, "таймаут на подключение к серверу")
+	flag.Parse()
 
+	//flag.Arg принимает аргументы п
+	host := os.Args[2]
+	port := os.Args[3]
+	conAdress := fmt.Sprint(host, ":", port)
+
+	//инициализирую канал для работы shut down
+	killChan := make(chan os.Signal, 1)
+	signal.Notify(killChan, os.Interrupt)
+
+	//передаем время для таймаута, адрес подключения и канал
+	clientTCP(*timeout, conAdress, killChan)
+
+}
+
+func clientTCP(timeout time.Duration, conAdress string, killChan <-chan os.Signal) {
+	//инициализирую нужные переменные
+	var flags = true
+	var err error
+	var conn net.Conn
+	//инициализирую таймер таймаута
+	timer := time.After(timeout)
+	tm := time.Now()
+
+	for flags {
+		select {
+		case <-timer:
+			fmt.Println("Время ожидания подключения вышло!")
+			fmt.Println("Ожидали:", time.Since(tm), conAdress)
+			flags = false
+			return
+		default:
+			conn, err = net.Dial("tcp", conAdress)
+			if err != nil {
+				time.Sleep(100 * time.Millisecond)
+				continue
+			}
+			flags = false
+			break
+		}
+	}
+
+	//в отдельной горутине следим за сигналом системы для завершения программы
+	go shutDown(killChan, conn)
+
+	//в бесконечном цикле пересылаем данные
+	for {
+		//читаем из stdin
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Input: ")
+		input , _ := reader.ReadString('\n')
+
+		//отправляем в stdout
+		fmt.Fprintf(conn, input +"\n")
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Printf("#%s\n", err)
+		}
+		fmt.Print("Ответ: ", message)
+		//conn.Close()
+	}
+}
+
+func shutDown(killChan <-chan os.Signal, conn net.Conn) {
+	select {
+	case <-killChan:
+		conn.Close()
+	}
 }
